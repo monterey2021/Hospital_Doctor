9ecca9df842799f796fd5d561e02cd60b4a3eb0f
Martín Reyes <rreyes1700@gmail.com>
2025-06-26 22:34:15 -0300

    Actualizando el acceso incial

--- index.php
+++ index.php
@@ -19,40 +19,46 @@ if ($_SESSION){
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <div class="cont-principal" id="acceso">
-            <div class="cont-logo" id="logo-acceso">
-                <img src="img/acceso.png" alt="logo de acceso" class="img-responsive"/>
-            </div>
-            <div class="cont-formdeacceso" id="form-acceso">
-                <form class="acceso-usuario" action="acceso.php" method="post">
-                    <div class="cont-titulodeacceso" id="inicio">
-                        <h1>Iniciar Sesión</h1>
+        <div class="container px-4 text-center">
+            <div class="row gx-5">
+                <div class="col">
+                    <div class="p-3">
+                        <img src="img/logoprincipal.png" alt="logo de acceso" class="img-responsive"/>
                     </div>
-                    <div class="cont-cajadetexto">
-                        <input type="text" name="usuario" required="" autofocus="" placeholder="Nombre de usuario"/>
-                        <img src="img/user.png" alt="usuario" class="icono-input" />
+                </div>
+                <div class="col">
+                    <div class="p-3">
+                        <form class="acceso-usuario" action="acceso.php" method="post">
+                            <div class="cont-titulodeacceso" id="inicio">
+                                <h1>Iniciar Sesión</h1>
+                            </div>
+                            <div class="cont-cajadetexto">
+                                <input type="text" name="usuario" required="" autofocus="" placeholder="Nombre de usuario"/>
+                                <img src="img/user.png" alt="usuario" class="icono-input" />
+                            </div>
+                            <div class="cont-cajadetexto">
+                                <input type="password" id="input-contraseña" name="contraseña" placeholder="Contraseña"/>
+                                <button type="button" class="toggle-password" onclick="mostrarContraseña()">
+                                    <img src="img/eye-open.png" alt="mostrar" id="icono-ojo" />
+                                </button>
+                            </div>
+                            <button class="cont-botonacceso" id="boton-acceso" type="submit">Acceder</button>
+                            <div class="cont-contraseña">
+                                <p class="help-block"><a href="/Agendamiento2024/autoregistro_index.php">Registrar</a></p>
+                                <p class="help-block"><a href="/Agendamiento2024/recuperar_index.php">¿Olvidaste tu contraseña?</a></p>
+                            </div>
+                                <?php if (!empty($_SESSION['error'])) { ?>
+                            <div class="alerta-flotante">
+                                <img src="img/alerta.png" alt="error" class="icono-alerta" />
+                                    <?php echo $_SESSION['error']; ?>
+                            </div>
+                                <?php } ?>
+                        </form>
                     </div>
-                    <div class="cont-cajadetexto">
-                        <input type="password" id="input-contraseña" name="contraseña" placeholder="Contraseña"/>
-                        <button type="button" class="toggle-password" onclick="mostrarContraseña()">
-                            <img src="img/eye-open.png" alt="mostrar" id="icono-ojo" />
-                        </button>
-                    </div>
-                    <button class="cont-botonacceso" id="boton-acceso" type="submit">Acceder</button>
-                    <div class="cont-contraseña">
-                        <p class="help-block"><a href="/Agendamiento2024/autoregistro_index.php">Registrar</a></p>
-                        <p class="help-block"><a href="/Agendamiento2024/recuperar_index.php">¿Olvidaste tu contraseña?</a></p>
-                    </div>
-                        <?php if (!empty($_SESSION['error'])) { ?>
-                    <div class="alerta-flotante">
-                        <img src="img/alerta.png" alt="error" class="icono-alerta" />
-                            <?php echo $_SESSION['error']; ?>
-                    </div>
-                        <?php } ?>
-                </form>
+                </div>
             </div>
         </div>
-        <script src="js/jquery-1.12.2.min.js"></script>
-        <script src="js/scripts.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
 </html>
